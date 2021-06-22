67a6dee66de8565471186049e48a8e46080fd16c
stoffee <chris@roadkill.org>
2021-06-22 13:09:53 -0700

    removed the extra )

--- gcp/main.tf
+++ gcp/main.tf
@@ -90,5 +90,5 @@ data "terraform_remote_state" "aws" {
 
 output "gcp_id" {
   description = "GCP Instance ID"
-  value       = google_compute_instance.default.id)
+  value       = google_compute_instance.default.id
 }
